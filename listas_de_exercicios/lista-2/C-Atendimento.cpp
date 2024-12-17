1cae2db0e2ad8dc95d8f2063274d0bb087647e8f
Vinícius Henrique <viniciushez@gmail.com>
2024-12-17 13:11:21 -0300

    Lista 2 - C

---
+++ listas_de_exercicios/lista-2/C-Atendimento.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long
+
+int main() {
+    fast_io
+
+    ll n, m, a, ans = 0;
+    priority_queue<vector<ll>, vector<vector<ll>>, greater<vector<ll>>> minCaixas;
+    vector<ll> v;
+    queue<ll> fila;
+    cin >> n >> m;
+
+    for (ll i = 0; i < n; i++) {
+        cin >> a;
+        v = {0, i, a};
+        minCaixas.push(v);
+    }
+
+    for (ll i = 0; i < m; i++) {
+        cin >> a;
+        fila.push(a);
+    }
+
+    vector<ll> aux;
+
+    while (!fila.empty()) {   // O(Mlog(N))
+        aux = minCaixas.top();
+        aux[0] += aux[2] * fila.front();
+        minCaixas.pop();
+        minCaixas.push(aux);
+        fila.pop();
+    }
+
+    // Agora preciso verificar o maior elemento da fila de prioridade restante
+
+    while (!minCaixas.empty()) {  // O(N)
+        if (minCaixas.top()[0] > ans)
+            ans = minCaixas.top()[0];
+        minCaixas.pop();
+    }
+
+    cout << ans << "\n";
+    return 0;
+}
+
+// O(Mlog(N))
--- template.cpp
+++ template.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 #define fast_io ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define ll long long
 
 int main() {
-    fast_io;
+    fast_io
     
+
     return 0;
 }
