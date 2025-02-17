a6eb3e544958b7724d23fa9b8216829bafdbc8fb
Vinícius Henrique <viniciushez@gmail.com>
2025-02-16 21:56:34 -0300

    algoritmos

---
+++ algoritmos/004-upper_bound.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int upper_bound_vini(vector<int> arr, int key) {
+    int l, r, mid, ans;
+    l = 0;
+    r = arr.size() - 1;
+    ans = -1;
+
+    while (l <= r) {
+        mid = (l + r) / 2;
+        if (arr[mid] > key) {
+            ans = mid;
+            r = mid - 1;
+        }
+        else
+            l = mid + 1;
+    }
+
+    return ans;
+}
